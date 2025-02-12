a54cec87a96e3569f7330c03c8634090fda5f657
Tronganh1611 <tronganh16112003@gmail.com>
2025-02-12 14:57:40 +0700

    xong lab2-bai1,2

---
+++ BTJava/src/Lab2/Bai1.java
@@ -0,0 +1,26 @@
+package Lab2;
+
+import java.util.Scanner;
+
+public class Bai1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhập biến a: ");
+		int a = scanner.nextInt();
+		System.out.print("Nhập biến b: ");
+		int b = scanner.nextInt();
+		if(a == 0){
+			if(b == 0){
+			System.out.print("Phương trình có vô số nghiệm");
+			}
+			else{ System.out.print("Phương trình vô nghiệm");
+			}
+		}
+		else {
+			double x=(double)-b/a;
+			System.out.printf("nghiệm của phương trình là x = %.2f", x);
+			
+		}
+	}
+
+}
