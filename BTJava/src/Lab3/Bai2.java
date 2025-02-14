fa47154a409346ce8d0ad639b6e6fbfe1edd9b65
Tronganh1611 <tronganh16112003@gmail.com>
2025-02-14 15:00:16 +0700

    Lab3.Bai2 done

---
+++ BTJava/src/Lab3/Bai2.java
@@ -0,0 +1,19 @@
+package Lab3;
+
+public class Bai2 {
+
+	public static void main(String[] args) {
+		int x;
+		for(int i=1; i<=9;i++) {
+			System.out.println();
+			System.out.printf("Bảng nhân %d",i);
+			System.out.println();
+		for(int j=1; j<=10;j++){
+			System.out.printf("%d x %d = %d", i, j, i*j);
+			System.out.println();
+			}
+		}
+	}
+
+
+}
