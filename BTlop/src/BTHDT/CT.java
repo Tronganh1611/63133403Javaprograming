0099f2e89ca90b20469f0dc41ccfba7674e0f95d
Tronganh1611 <tronganh16112003@gmail.com>
2025-02-16 22:19:15 +0700

    sua loi file CT

--- BTlop/src/BTHDT/CT.java
+++ BTlop/src/BTHDT/CT.java
@@ -1,6 +1,5 @@
 package BTHDT;
 
-import java.util.ArrayList;
 
 import BTHDT.Sanpham;
 
@@ -17,25 +16,9 @@ public class CT {
 		//in thong tin
 		String Thongtinsp1=sp.toString();
 		String Thongtinsp2 = "Sanpham [maSp=" + sp1.maSp + ", tenSp=" + sp1.tenSp + ", loaiSp=" + sp1.loaiSp + ", anhSp=" + sp1.anhSp + "]";
-//		System.out.println(Thongtinsp1);
-//		System.out.println(Thongtinsp2);
-		ArrayList<Sanpham> dsSp;
-		//Tao  moi
-		dsSp = new ArrayList<Sanpham>();
+		System.out.println(Thongtinsp1);
+		System.out.println(Thongtinsp2);
 		
-		//Nhap 3 san pham
-		Sanpham sp2 = new Sanpham(2, "Quan Jean", "Quan" ,"qj.jpg");
-		Sanpham sp3 = new Sanpham(3, "Quan Jean", "Quan" ,"qj.jpg");
-		Sanpham sp4 = new Sanpham(4, "Quan Jean", "Quan" ,"qj.jpg");
-		//Them san pham vao ds
-		dsSp.add(sp);
-		dsSp.add(sp1);
-		dsSp.add(sp2);
-		dsSp.add(sp3);
-		dsSp.add(sp4);
-		
-		for (Sanpham x : dsSp)
-			System.out.println(sp.toString());
 	}
 
 }
